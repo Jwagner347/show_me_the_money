821f2456502a2a3e129864734f0b7f089134571d
Jeff Wagner <jwagner@thoughtworks.com>
2015-05-06 21:33:30 -0700

    scoping the root

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
 
 
   devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations" }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'devise/registrations#new'
-
+  devise_scope :user do
+    root to: 'users/registrations#new'
+  end
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
